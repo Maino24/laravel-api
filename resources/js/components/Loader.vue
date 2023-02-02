e8130f508545c261087e822023aad434bc667da8
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-02 22:40:23 +0100

    posizionamento centrale spinner

--- resources/js/components/Loader.vue
+++ resources/js/components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="loader">
         <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
         </div>
@@ -11,4 +11,19 @@ export default{
     name: 'Loader'
 }
 </script>
+
+<style scoped lang="scss">
+//oscura lo sfondo e posiziona loo spinner in centro alla pagina al caricamento
+#loader {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
 
